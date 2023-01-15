46bd9e50f85d530acb56d4a3241a19eba4cdf98f
ferhatosanoglu <ferhatosanogluo@gmail.com>
2023-01-15 17:17:29 +0300

    fix : update env info

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
-import environment from './environments/environment';
 import { BrandSchema } from './models';
 import { BrandModule } from './modules/brand/brand.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forFeature([{ name: 'Brand', schema: BrandSchema }]),
-    MongooseModule.forRoot(environment.MONGODB_URI),
     BrandModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
